88074fb075867a7b5fe2eb35065e27fb7f1b6df2
ccvanginkel <34521994+ccvanginkel@users.noreply.github.com>
2021-10-05 19:20:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function makePizza () {
+    console.log("koop pizza in de supermarkt");
+    console.log("verwarm de oven voor op 220 graden");
+    console.log("haal de pizza uit de verpakking en verwarm gedurende 8 minuten in de oven");
+}
+
+// 7 werkt niet want de functie to call de functie moet eerst gechreven worden
+
+function makeSushi () {
+    console.log("koop sushi in de supermarkt");
+    console.log("verwarm de oven voor op 100 graden");
+    console.log("haal de sushi uit de verpakking en regeneer gedurende 8 minuten in de oven");
+}
+
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+makePizza();
